3f9fa9cca6d73e3c0d1b3f941e83d20e5bc12c7c
Kevin Wierman <kwierman@gmail.com>
2017-04-18 11:41:57 -0500

    More Initial Commit

---
+++ {{cookiecutter.project_slug}}/README.md
@@ -0,0 +1,15 @@
+# {{cookiecutter.project_name}}
+
+
+[![Updates](https://pyup.io/repos/github/{{cookiecutter.github_group}}.lower()/{{cookiecutter.project_slug}}/shield.svg)](https://pyup.io/repos/github/{{cookiecutter.github_group}}.lower()/{{cookiecutter.project_slug}}/)
+[![Python 3](https://pyup.io/repos/github/{{cookiecutter.github_group}}.lower()/{{cookiecutter.project_slug}}/python-3-shield.svg)](https://pyup.io/repos/github/{{cookiecutter.github_group}}.lower()/{{cookiecutter.project_slug}}/)
+[![Build Status](https://travis-ci.org/{{cookiecutter.github_group}}/{{cookiecutter.project_slug}}.svg?branch=master)](https://travis-ci.org/{{cookiecutter.github_group}}/{{cookiecutter.project_slug}})
+[![Documentation Status](https://readthedocs.org/projects/{{cookiecutter.project_slug}}/badge/?version=latest)](https://{{cookiecutter.project_slug}}.readthedocs.io/en/latest/?badge=latest)
+[![pypi](https://img.shields.io/pypi/v/{{cookiecutter.project_slug}}.svg)](https://pypi.python.org/pypi/{{cookiecutter.project_slug}})
+
+
+{{cookiecutter.project_short_description}}
+
+* Open Source License: {{cookiecutter.open_source_license}}
+* Documentation: https://{{cookiecutter.project_slug}}.readthedocs.io.
+
---
+++ {{cookiecutter.project_slug}}/setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements("requirements.txt")
+reqs = [str(ir.req) for ir in install_reqs]
+
+test_reqs = parse_requirements("requirements.txt")
+test_reqs = [str(ir.req) for ir in test_reqs]
+
+
+{%- set license_classifiers = {
+    'MIT license': 'License :: OSI Approved :: MIT License',
+    'BSD license': 'License :: OSI Approved :: BSD License',
+    'ISC license': 'License :: OSI Approved :: ISC License (ISCL)',
+    'Apache Software License 2.0': 'License :: OSI Approved :: Apache Software License',
+    'GNU General Public License v3': 'License :: OSI Approved :: GNU General Public License v3 (GPLv3)'
+} %}
+
+setup(
+    name='{{ cookiecutter.project_slug }}',
+    version='{{ cookiecutter.version }}',
+    description="{{ cookiecutter.project_short_description }}",
+    long_description="TODO: Fill in",
+    author="{{ cookiecutter.full_name.replace('\"', '\\\"') }}",
+    author_email='{{ cookiecutter.email }}',
+    url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}',
+    packages=[
+        '{{ cookiecutter.project_slug }}',
+    ],
+    package_dir={'{{ cookiecutter.project_slug }}':
+                 '{{ cookiecutter.project_slug }}'},
+    entry_points={
+        'console_scripts': [
+            '{{ cookiecutter.project_slug }}={{ cookiecutter.project_slug }}.cli:main'
+        ]
+    },
+    include_package_data=True,
+    install_requires=reqs,
+{%- if cookiecutter.open_source_license in license_classifiers %}
+    license="{{ cookiecutter.open_source_license }}",
+{%- endif %}
+    zip_safe=False,
+    keywords='{{ cookiecutter.project_slug }}',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+{%- if cookiecutter.open_source_license in license_classifiers %}
+        '{{ license_classifiers[cookiecutter.open_source_license] }}',
+{%- endif %}
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_reqs
+)
